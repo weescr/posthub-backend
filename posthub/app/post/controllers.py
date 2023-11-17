0ad5b5942a7db917876377a3e8b14231304fcf12
Zuko1337 <zuko13371@yandex.ru>
2023-11-17 20:21:07 +0300

    контроллеры для поста

---
+++ posthub/app/post/controllers.py
@@ -0,0 +1,28 @@
+from fastapi import APIRouter, Depends
+from sqlalchemy.ext.asyncio import AsyncSession
+from posthub.db import get_session
+
+from posthub.services import post as PostService
+from posthub.dto import post as PostDTO
+
+router = APIRouter()
+
+
+@router.post("/post")
+async def create_post(db: AsyncSession = Depends(get_session), data: PostDTO.Post = None):
+    return await PostService.create_post(data=data, db=db)
+
+
+@router.get("/post/{id}")
+async def get_post(db: AsyncSession = Depends(get_session), id: int = None):
+    return await PostService.get_post(id=id, db=db)
+
+
+@router.put("/post/{id}")
+async def update_post(id: int, db: AsyncSession = Depends(get_session), data: PostDTO.Post = None):
+    return await PostService.update_post(data=data, db=db, id=id)
+
+
+@router.delete("/post/{id}")
+async def delete_post(db: AsyncSession = Depends(get_session), id: int = None):
+    return await PostService.remove_post(db=db, id=id)
