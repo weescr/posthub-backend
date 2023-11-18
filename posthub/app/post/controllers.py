5a18be50c474118a7b5009e7137a628ba068e527
Zuko1337 <zuko13371@yandex.ru>
2023-11-18 18:53:35 +0300

    добавил ручку

--- posthub/app/post/controllers.py
+++ posthub/app/post/controllers.py
@@ -18,6 +18,11 @@ async def get_post(db: AsyncSession = Depends(get_session), id: int = None):
     return await PostService.get_post(id=id, db=db)
 
 
+@router.get("/post/title/{title}")
+async def get_post_id_by_title(title: str, db: AsyncSession = Depends(get_session)):
+    return await PostService.get_post_id_by_title(title=title, db=db)
+
+
 @router.put("/post/{id}")
 async def update_post(id: int, db: AsyncSession = Depends(get_session), data: PostDTO.Post = None):
     return await PostService.update_post(data=data, db=db, id=id)
