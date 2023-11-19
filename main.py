399fc074bab6327492ba405499bb5df9ed25e95b
Zuko1337 <zuko13371@yandex.ru>
2023-11-19 17:11:22 +0300

    обновил main

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ from fastapi.middleware.cors import CORSMiddleware
 from fastapi.responses import ORJSONResponse
 
 import posthub.app.healthcheck.controllers as healthcheck
+from posthub.app.post import controllers as PostControllers
 from posthub import exceptions
 from posthub.db import get_session
 from posthub.logger import logger
@@ -24,6 +25,7 @@ app.add_middleware(
 )
 
 app.include_router(healthcheck.router, tags=["healthcheck"])
+app.include_router(PostControllers.router, tags=["post"])
 
 
 @app.exception_handler(Exception)
@@ -53,5 +55,4 @@ async def unicorn_api_exception_handler(request: Request, exc: exceptions.ApiExc
 if __name__ == "__main__":
     import uvicorn
 
-    uvicorn.run("main:app", host="0.0.0.0", port=5000)
-
+    uvicorn.run("main:app", host="localhost", port=5000)
