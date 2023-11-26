1231e015535125c612bd1a74bbedd19b3c092884
Zuko1337 <zuko13371@yandex.ru>
2023-11-26 20:00:19 +0300

    Обновил ручки

--- posthub/app/auth/controllers.py
+++ posthub/app/auth/controllers.py
@@ -1,12 +1,43 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, Depends
+from guardpost.authentication import Identity
+
 from posthub.app.auth.models import User as UserService
-from .views import UserLoginView, UserRegistrationView, UserUpdateView, UserGetProfileView
+from .views import (
+    UserLoginView,
+    UserRegistrationView,
+    UserUpdateView,
+    TokenRequestView,
+    UserProfileView
+)
+from posthub.exceptions import (
+    UserAlreadyRegistered,
+    UserNotFoundError,
+    PasswordMatchError
+)
 from posthub.db.connection import Transaction
 from posthub.protocol import Response
-from posthub.exceptions import UserAlreadyRegistered, UserNotFoundError, PasswordMatchError
-
+from posthub.auth.handlers import decode_token, generate_tokens, AuthHandler
+from posthub.auth.hash import get_password_hash
 
 router = APIRouter()
+
+
+@router.post("/user/auth/refresh_token")
+async def refresh_token(body: TokenRequestView):
+    token = decode_token(body.refresh_token)
+    async with Transaction():
+        user = await UserService.get_user_by_id(token.sub)
+
+    if not user:
+        raise UserNotFoundError
+
+    return Response(
+        message="Токен успешно обновлен",
+        payload=generate_tokens(
+            sub=str(user.id),
+            username=user.username
+        )
+    )
 
 
 @router.post("/user/auth/registration")
@@ -19,7 +50,7 @@ async def create_user(data: UserRegistrationView):
         user_id = await UserService.create_user(data=data)
     return Response(
         message="Пользователь успешно создан",
-        payload={'user_id': user_id, 'username': data.username}
+        payload=generate_tokens(sub=str(user_id), username=data.username)
     )
 
 
@@ -32,13 +63,9 @@ async def update_user_data(data: UserUpdateView, id: int):
             raise UserNotFoundError
 
         updated_user = await UserService.update_user_info(data=data, user_id=id)
-        updated_payload = {
-            "tg_channel": updated_user.tg_channel,
-            "email": updated_user.email_adress
-        }
     return Response(
         message="Информация о пользователе обновлена",
-        payload=updated_payload
+        payload=generate_tokens(sub=str(updated_user.id), username=updated_user.username)
     )
 
 
@@ -46,38 +73,41 @@ async def update_user_data(data: UserUpdateView, id: int):
 async def login_user(data: UserLoginView):
     async with Transaction():
         user = await UserService.get_user_by_username(username=data.username)
+
     if not user:
         raise UserNotFoundError
 
-    if user.password == data.password:
+    hashed_password = get_password_hash(data.password)
+
+    if user.password == hashed_password:
         return Response(
-            message="Авторизация успешна!",
-            payload={
-                "username": user.username
-            }
+            payload=generate_tokens(
+                sub=str(user.id),
+                username=user.username,
+            )
         )
     raise PasswordMatchError
 
 
 @router.post("/user/profile")
-async def get_profile(data: UserGetProfileView):
+async def get_profile(data: UserProfileView):
     async with Transaction():
-        user = await UserService.get_user_by_username(data.username)
-        if not user:
+        user_data = await UserService.get_user_by_username(data.username)
+        if not user_data:
             raise UserNotFoundError
 
-        return Response(
+        return Response[UserProfileView](
             message="Профиль существует!",
             payload={
-                "username": user.username,
-                "tg_channel": user.tg_channel,
-                "email": user.email_adress
+                "username": user_data.username,
+                "tg_channel": user_data.tg_channel,
+                "email": user_data.email_adress
             }
         )
 
 
 @router.delete("/user/profile")
-async def delete_profile(id: int):
+async def delete_profile(user_token: int):
     async with Transaction():
-        await UserService.delete_user(id=id)
+        await UserService.delete_user(id=user_token)
     return Response()
