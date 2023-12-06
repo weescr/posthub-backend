39e25b90b8581152502085b56f0994938496f9da
Zuko1337 <zuko13371@yandex.ru>
2023-12-06 22:26:02 +0300

    Добавил class Config в PostView

--- posthub/app/posts/views.py
+++ posthub/app/posts/views.py
@@ -2,8 +2,11 @@ from pydantic import BaseModel
 from datetime import datetime
 
 
-class Post(BaseModel):
+class PostView(BaseModel):
     title: str
     description: str
     content: str
     publication_date: datetime
+
+    class Config:
+        orm_mode = True
